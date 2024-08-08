204934060c311e0500f3370b79b1d0fbbc741820
Kurt <134671520+DaBloat@users.noreply.github.com>
2024-08-08 23:16:17 +0800

    🔊 Volume notification Script 🔊

---
+++ .scripts/volume.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Created by DaBloat
+case $1 in
+	u)
+		wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
+		wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+
+		;;
+	d)
+		wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+		;;
+	m)
+		wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle 
+		;;
+esac
+
+STATUS=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $NF;}')
+
+notification(){
+	case $STATUS in
+		"[MUTED]")
+			dunstify "Volume is in muted" -r 1111 -i "volume-mute" -t 2000
+			;;
+		*)
+			VOLUME=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | tr -dc '0-9' | sed 's/^0\{1,2\}//')
+			if [ "$VOLUME" -lt 35 ];
+			then
+				ICON="off";
+			elif [ "$VOLUME" -lt 65 ];
+			then
+				ICON="min";
+			else 
+				ICON="high";
+			fi
+                      dunstify "Volume @ ${VOLUME}%" -r 1111 -h int:value:"$VOLUME" -i "volume-$ICON" -t 2000
+	esac
+}
+
+notification
+
+
