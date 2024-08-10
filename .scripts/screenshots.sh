72c3050e33cc179b105fa71ba39a3060bce71dad
Kurt <134671520+DaBloat@users.noreply.github.com>
2024-08-11 01:37:40 +0800

    📸 Screenshot script 📸

--- .scripts/screenshots.sh
+++ .scripts/screenshots.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+
+FILE_NAME=~/Pictures/screenshots/$(date +'%s.png')
 case $1 in
-	p)
+	pc)
+		grim -g "$(slurp -w 0)" - | wl-copy
+		dunstify "Screenshot Copied!" -i "screenshot-copy" -r 1113 -t 3000
 		;;
 	w)
-		grim ~/Pictures/$(date +'%s.png')
+		grim $FILE_NAME
+		dunstify "Screenshot Saved!" "@$FILE_NAME" -i "$FILE_NAME" -r 1113 -t 3000
+		;;
+	p)
+
+		grim -g "$(slurp -w 0)" $FILE_NAME
+		dunstify "Screenshot Saved!" "@$FILE_NAME" -i "$FILE_NAME" -r 1113 -t 3000
+		;;
+	wc)
+		grim - | wl-copy
+		dunstify "Screenshot Copied!" -i "screenshot-copy" -r 1113 -t 3000
 		;;
 
 esac
+
 
 
 
